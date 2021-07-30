c5792e7724ec20cb1fc2e2c8953eace02041b4fe
Hrithik Sagar <hrithiksagar36@gmail.com>
2021-07-30 10:41:34 +0530

    update

--- Questions and Solution/29th July/tempCodeRunnerFile.py
+++ Questions and Solution/29th July/tempCodeRunnerFile.py
@@ -1,4 +1,4 @@
-# word1 = input()#Enter word1
+word1 = input()#Enter word1
 # word2 = input()#Enter word2
 # word3 = input()#Enter word3
 # vowels = ['a','e','i','o','u', 'A', 'E', 'I', 'O', 'U']
@@ -14,22 +14,3 @@
 #     else:
 #         word3 = word3.replace(word3[i], word3[i].upper())
 # print(word1+word2+word3)
-
-#====
-word1 = input()#Enter word1
-word2 = input()#Enter word2
-word3 = input()#Enter word3
-vowels = ['a','e','i','o','u', 'A', 'E', 'I', 'O', 'U']
-wd3  =""
-for i in range(len(word1)):
-    if word1[i] in vowels:
-        word1 = word1.replace(word1[i], '%')
-for i in range(len(word2)):
-    if word2[i] not in vowels:
-        word2 = word2.replace(word2[i], '#')
-for i in range(len(word3)):
-    if i%2==0:
-        wd3 = wd3+word3[i].lower()
-    else:
-        wd3 = wd3+word3[i].upper()
-print(word1+word2+wd3)
