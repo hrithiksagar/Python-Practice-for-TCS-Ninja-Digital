6ae02f86323f3caa349fe84d699d3fcf0bd8b53c
Hrithik Sagar <hrithiksagar36@gmail.com>
2021-08-05 11:19:40 +0530

    Update

--- Questions and Solution/5th Aug/Sum of Consecutive numbers.py
+++ Questions and Solution/5th Aug/Sum of Consecutive numbers.py
@@ -4,13 +4,27 @@
 # arr[] = {1,3,2,5,6}
 # m =3
 # so max sum of 3 consecutive elements is 2+5+6;
-# import random
-# res = random.sample(range(1,50),7)
-# print(res)
-# n = int(input("Enter a number to start summation: "))
+
 # for i in range(len(=res)):
 #     sum = 0
 #     sum += res[i]
 #User input array without knowing size:
 # li = [int(i) for i in input().split()] # using List Comp.
 # print(li)
+
+import random
+res = random.sample(range(1,50),7)
+print(res)
+n = int(input("Enter a number to start summation: "))
+
+# vineeth
+# n = int(input(""))
+# n = int(input))
+# arr = []
+# for i in range(n):
+#     x = int(input())
+#     arr.append()
+# s = 0
+# max = 
+
+# Shriyans
