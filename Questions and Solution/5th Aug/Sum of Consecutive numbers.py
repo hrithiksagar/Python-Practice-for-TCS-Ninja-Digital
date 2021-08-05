764df09b091f668bc57eec2137babd281ba450a9
Hrithik Sagar <hrithiksagar36@gmail.com>
2021-08-05 11:12:58 +0530

    Update

---
+++ Questions and Solution/5th Aug/Sum of Consecutive numbers.py
@@ -0,0 +1,16 @@
+# given a set of array elements
+#task : find max sum of m consecutive elements
+# suppose:
+# arr[] = {1,3,2,5,6}
+# m =3
+# so max sum of 3 consecutive elements is 2+5+6;
+# import random
+# res = random.sample(range(1,50),7)
+# print(res)
+# n = int(input("Enter a number to start summation: "))
+# for i in range(len(=res)):
+#     sum = 0
+#     sum += res[i]
+#User input array without knowing size:
+# li = [int(i) for i in input().split()] # using List Comp.
+# print(li)
