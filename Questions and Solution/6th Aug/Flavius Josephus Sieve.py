b14aedb4f07fc9a1ece1c1cb3339fed8886f4d6d
Hrithik Sagar <hrithiksagar36@gmail.com>
2021-08-06 11:19:08 +0530

    Update others sol for strong number

---
+++ Questions and Solution/6th Aug/Flavius Josephus Sieve.py
@@ -0,0 +1,6 @@
+# First differences of Flavius Josephus's sieve.
+# Find the nth difference of Flavius Josephus's Sieve
+# D(n) = FJ(n+1) - FJ(n).
+# EXAMPLE
+# Flavius's sieve starts 1,3,7,13,19,27,39,49
+# 5th difference= 6th Term-5th Term=27-19=8
