fd8bbca802997bf018fd0d68803618dedf5c79d8
Hrithik Sagar <hrithiksagar36@gmail.com>
2021-08-16 13:17:36 +0530

    update clever thief

--- Questions and Solution/16th Aug/Clever thief.py
+++ Questions and Solution/16th Aug/Clever thief.py
@@ -21,11 +21,36 @@ if len of array is 0, print 0
 if len of array is 1, print 1st ele.
 if len is 2, print max of those 2 elem
 3. update dp[0] as array[0] and dp[1] as max of 2 ele
-4. traverse array form 2nd ele i.e. 2nd index to end
-5. for every index, update dp[i] as max dp[i-2]+arrau[0] and dp[i-1]
-this defines 2 cases:
-if ele is selected then previous ele cant selected.
-if ele not selected then porevious ele can be selected
-6. print dp[n-10
+4. traverse array form 2nd ele i.e. 2nd index to end of array
+5.
+for every index, update dp[i] as max("dp[i-2]+arrau[0]" and "dp[i-1]")
+this step defines 2 cases:
+(i)  if ele is selected then previous ele cant selected.
+(ii) if ele is not selected then previous ele can be selected
+6. print dp[n-1]
 
 """
+
+
+def maxloot(h, n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return h[0]
+    if n == 2:
+        return max(h[0], h[1])
+    # dp[i] shows max stolen value, for after reaching house i
+    dp = [0] * n  # creating empty list of size
+    # lets initialize first 2 indexes of dp
+    dp[0] = h[0]
+    dp[1] = max(h[0], h[1])
+
+    # now lets fill the remaining portions
+    for i in range(2, n):
+        dp[i] = max(h[i] - dp[i - 2], dp[i - 1])
+    return dp[-1]
+
+
+h = list(map(int, input().split()))
+n = len(h)
+print("Maximum loot value = ", maxloot(h, n))
